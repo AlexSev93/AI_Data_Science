c2f4e92a7e523d77ce8700db00feef257578c49c
AlexSev93 <sevosanovjob@gmail.com>
2023-10-13 20:52:02 +0300

    3

---
+++ 3_Numpy/Lite.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+my_array = np.loadtxt('train_vector_1.csv')
+
+# z1
+# def mean(array):
+#     s = 0
+#     for i in array:
+#         s += i
+#     return s/len(array)
+#
+#
+# print(mean(my_array))
+# print(mean(list(my_array)))
+
+# my_array_2 = np.loadtxt('iris.csv')
+#
+# print(type(my_array_2))
