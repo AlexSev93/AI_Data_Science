1b2692e3482e882b0d375842a63c096ba14da5c9
AlexSev93 <sevosanovjob@gmail.com>
2023-12-08 12:54:01 +0300

    panda2 l

---
+++ 6_2_Pandas/Lite.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('C:/Users/Admin/PycharmProjects/AI_Data_Science/6_2_Pandas/googleplaystore.csv')
+
+# z1
+# categories = list(data['Category'].unique())
+# for category in categories:
+#     print(f'{category} ---> {data[data.Category==category].shape[0]}')
+
+# z2
+# data_sort = data.sort_values(by='Rating', ascending=False)
+# print(data_sort)
+
+# z3
+# data = data[(data.Rating >= 3) & (data.Rating <= 5)]
+# free_app = data[data.Type == 'Free'].shape[0]
+# paid_app = data[data.Type == 'Paid'].shape[0]
+# print(f'платные-->{paid_app}, бесплатные-->{free_app}')
+
+# z4
+new_index = list(np.random.randint(0, data.shape[0], data.shape[0]))
+print(data.head())
+data.index = new_index
+print(data.head())
