3c53ab1e27afd7c70b1b54d6349b70fdd03d17ae
User <sevosanovjob@gmail.com>
2023-12-08 14:18:52 +0300

    pandas up

--- 6_2_Pandas/UltraPro.py
+++ 6_2_Pandas/UltraPro.py
@@ -3,6 +3,7 @@ import pandas as pd
 data = pd.read_csv('data.csv', encoding='cp1250')
 data['TotalCost'] = data.Quantity * data.UnitPrice
 data.index = pd.to_datetime(data.InvoiceDate)
+data.drop('InvoiceDate', axis=1,  inplace=True)
 
 # z1
 # z1 = data.TotalCost.resample('1W').sum()
@@ -19,3 +20,12 @@ data.index = pd.to_datetime(data.InvoiceDate)
 # print(z3)
 
 # z4
+# cards = pd.read_csv('card_data.csv', encoding='cp1250')
+# cards = cards[::100]
+# data = data[::100]
+# data = data.merge(cards, how='left', on='CustomerID')
+# print(data.head())
+
+# z5
+# z5 = data[['UnitPrice', 'TransactionNum', 'Quantity']].quantile([0.4, 0.6])
+# print(z5)
