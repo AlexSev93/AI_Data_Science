87b6d81b4531d939ea7b523c63d698d85df5ab7a
AlexSev93 <sevosanovjob@gmail.com>
2024-01-09 15:40:36 +0300

    panda2 l

---
+++ 8_Logic/Pro.py
@@ -0,0 +1,24 @@
+from combinatorica import (fact, Cnm,
+                           Cnm_p, Per_p,
+                           Rnm, Rnm_p)
+
+# z1
+# answer = Cnm(4, 1) * (Cnm(4, 1)+Cnm(4, 1)+Cnm(4, 1)+Cnm(4, 1))
+# print(answer)
+
+# z2
+# a = Cnm(5, 4) * fact(4)
+# print(a)
+# b = Cnm(5,1) + Cnm(5,2) + Cnm(5,3) + Cnm(5,4) + Cnm(5,5)
+# print(b)
+# c = Rnm(5,2)
+# print(c)
+
+# z3
+# a = Cnm(11,1)
+# print(a)
+# b = Cnm(11,2) * Rnm(3,2)
+# print(b)
+# c = Rnm(11,3)
+# d = Cnm(11, 1)**3
+# print(d)
