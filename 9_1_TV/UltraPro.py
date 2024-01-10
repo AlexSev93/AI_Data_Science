daf94e42615adbe36db1780cb2827aed399c01b1
User <sevosanovjob@gmail.com>
2024-01-10 14:29:44 +0300

    9_TV_1

---
+++ 9_1_TV/UltraPro.py
@@ -0,0 +1,31 @@
+# z1
+# (1-0.6)(1-x)=0.06
+# 0.4-0.4x = 0.06
+# 0.4x = 0.36
+# x = 1-(0.06/(1-0.6))
+# print(x)
+
+# z2
+# p = {0: 44/60, 150: 14/60, 1200: 2/60}
+# m = 0
+# for i in p.keys():
+#     m = m + p[i]*i
+# print(m)
+
+# z3
+# W = {-2: 0.4, 0: 0.1, 3: 0.3, 7: 0.2}
+# M = sum([key * W[key] for key in W])
+# D = sum([key ** 2 * W[key] for key in W]) - M ** 2
+# Mid = D ** (1 / 2)
+# print(M, D, Mid)
+
+# z4
+# a = 1/7
+# t = 1/15
+#
+# print(a*3)
+# print(t+a - t*a)
+#
+#
+# z5
+# print(2/(8-1))
